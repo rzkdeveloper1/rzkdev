64d7bc8660d6fb3e77eea2856d9fa903ee574f82
rzkdeveloper1 <86886256+rzkdeveloper1@users.noreply.github.com>
2021-07-05 00:21:21 +0700

    update login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+const benar-usr = document.querySelector('.container .username');
+const benar-pasa = document.querySelector('.container .password');
+
+if(benar-usr == test){
+	result.style.color = 'green';
+	result.innerHTML = "Username Benar"
+}
