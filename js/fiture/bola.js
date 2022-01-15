91b4cbc2ee3a5d882fa35c67a75af8cbc8014c48
rzkdeveloper1 <86886256+rzkdeveloper1@users.noreply.github.com>
2022-01-15 23:55:33 +0700

    update bola.js

--- js/fiture/bola.js
+++ js/fiture/bola.js
@@ -10,4 +10,4 @@ let club = [
 console.log(`${club[Math.floor(Math.random() * club.length)]}`)
 
 const element = document.getElementById("club1");
-element.innerHTML = "${club[Math.floor(Math.random() * club.length)]}";
+element.innerHTML = "iya";
