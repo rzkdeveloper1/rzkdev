e40a95f40f75790b884301c84b894def1bec9bac
rzkdeveloper1 <86886256+rzkdeveloper1@users.noreply.github.com>
2022-01-15 23:43:32 +0700

    update bola.js

---
+++ js/fiture/bola.js
@@ -0,0 +1,12 @@
+let club = [
+    "RB Jasbur",
+    "FK Herusia",
+    "Nesse",
+    "Temsoia",
+    "Victora",
+    "Manchester Gold"
+];
+
+console.log(`${club[Math.floor(Math.random() * club.length)]}`)
+
+document.getElementById("club1").innerHTML = `${club[Math.floor(Math.random() * club.length)]}`;
